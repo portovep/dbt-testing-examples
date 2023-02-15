f443974006be2a9d2e0d5831f071442cf944d6ed
Pablo <porto.vga@gmail.com>
2023-02-15 09:29:28 +0100

    First unit test for weight staging model

---
+++ tests/stg_gymapp__weight_unit_test.sql
@@ -0,0 +1,23 @@
+{{ config(tags=['unit-test']) }}
+
+{% call dbt_unit_testing.test('stg_gymapp__weight',
+    'it should cast string date to date') %}
+    
+  {% call dbt_unit_testing.mock_ref('raw_weight', {"input_format": "csv"}) %}
+
+    date,user_id,weight,measurement_unit
+    01/07/2009,1,82.50,'kg'
+  
+  {% endcall %}
+
+  {% call dbt_unit_testing.expect() %}
+  
+    select
+        to_date('2009-07-01', 'YYYY-MM-DD') as created_date,
+        82.50 as weight,
+        1 as user_id,
+        'kg' as measurement_unit
+  
+  {% endcall %}
+
+{% endcall %}
