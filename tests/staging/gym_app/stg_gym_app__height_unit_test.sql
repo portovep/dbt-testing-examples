85ff3a7d2098094c7fa560bca2d315936f362abe
Pablo <porto.vga@gmail.com>
2023-02-16 09:41:51 +0100

    Add unit test to rename height staging columns

--- tests/staging/gym_app/stg_gym_app__height_unit_test.sql
+++ tests/staging/gym_app/stg_gym_app__height_unit_test.sql
@@ -17,3 +17,27 @@
   {% endcall %}
 
 {% endcall %}
+
+UNION ALL
+
+{% call dbt_unit_testing.test('stg_gym_app__height',
+    'it should clean column names') %}
+    
+  {% call dbt_unit_testing.mock_source('gym_app', 'raw_height', {"input_format": "csv"}) %}
+
+    date,user_id,height,height_unit
+    '01/07/2009',1,180,'cm'
+  
+  {% endcall %}
+
+  {% call dbt_unit_testing.expect() %}
+  
+    select
+        to_date('2009-07-01', 'YYYY-MM-DD') as created_date,
+        1 as user_id,
+        180 as height,
+        'cm' as measurement_unit
+        
+  {% endcall %}
+
+{% endcall %}
