8f05c01af516ce0bccd01cff279d45535de347a2
Pablo <porto.vga@gmail.com>
2023-02-17 09:24:50 +0100

    Add unit test to convert weight from pounds to kilograms

--- tests/staging/gym_app/stg_gym_app__weight_unit_test.sql
+++ tests/staging/gym_app/stg_gym_app__weight_unit_test.sql
@@ -21,3 +21,27 @@
   {% endcall %}
 
 {% endcall %}
+
+UNION ALL
+
+{% call dbt_unit_testing.test('stg_gym_app__weight',
+    'it should convert pounds to kilograms') %}
+    
+  {% call dbt_unit_testing.mock_source('gym_app', 'raw_weight', {"input_format": "csv"}) %}
+
+    date,user_id,weight,measurement_unit
+    '01/07/2009',1,176.37,'pounds'
+  
+  {% endcall %}
+
+  {% call dbt_unit_testing.expect() %}
+  
+    select
+        to_date('2009-07-01', 'YYYY-MM-DD') as created_date,
+        80 as weight,
+        1 as user_id,
+        'kg' as measurement_unit
+  
+  {% endcall %}
+
+{% endcall %}
