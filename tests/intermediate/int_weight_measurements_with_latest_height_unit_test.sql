7d5ad1bc828b1645dd6f6ba9e3a75472cc6de08c
Pablo <porto.vga@gmail.com>
2023-02-17 09:43:26 +0100

    Add unit test to enrich each weight measurement with specific height measurement for the same user

--- tests/intermediate/int_weight_measurements_with_latest_height_unit_test.sql
+++ tests/intermediate/int_weight_measurements_with_latest_height_unit_test.sql
@@ -1,7 +1,7 @@
 {{ config(tags=['unit-test']) }}
 
 {% call dbt_unit_testing.test ('int_weight_measurements_with_latest_height',
-    'should enrich weight measurement with latest height measurement') %}
+    'it should enrich weight measurement with latest height measurement') %}
     
   {% call dbt_unit_testing.mock_ref ('stg_gym_app__weight') %}
     select
@@ -28,6 +28,35 @@
         1 as user_id,
         82.50 as weight,
         183 as height
+  {% endcall %}
+
+{% endcall %}
+
+UNION ALL
+
+{% call dbt_unit_testing.test ('int_weight_measurements_with_latest_height',
+    'it should enrich weight with null height when height measurement does not exist') %}
+    
+  {% call dbt_unit_testing.mock_ref ('stg_gym_app__weight') %}
+    select
+        to_date('2022-09-01', 'YYYY-MM-DD') as created_date,
+        1 as user_id,
+        82.50 as weight
+  {% endcall %}
+
+  {% call dbt_unit_testing.mock_ref ('stg_gym_app__height') %}
+    select
+        to_date('2021-07-01', 'YYYY-MM-DD') as created_date,
+        2 as user_id,
+        182 as height
+  {% endcall %}
+
+  {% call dbt_unit_testing.expect() %}
+    select
+        to_date('2022/09/01', 'YYYY-MM-DD') as created_date,
+        1 as user_id,
+        82.50 as weight,
+        null as height
   {% endcall %}
 
 {% endcall %}
