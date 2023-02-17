85e44b84cbbe09a3abb9f104f49d1713d3339a0b
Pablo <porto.vga@gmail.com>
2023-02-17 09:35:58 +0100

    Add unit test to enrich each weight measurement with the latest height measurement

---
+++ tests/intermediate/int_weight_measurements_with_latest_height_unit_test.sql
@@ -0,0 +1,33 @@
+{{ config(tags=['unit-test']) }}
+
+{% call dbt_unit_testing.test ('int_weight_measurements_with_latest_height',
+    'should enrich weight measurement with latest height measurement') %}
+    
+  {% call dbt_unit_testing.mock_ref ('stg_gym_app__weight') %}
+    select
+        to_date('2022-09-01', 'YYYY-MM-DD') as created_date,
+        1 as user_id,
+        82.50 as weight
+  {% endcall %}
+
+  {% call dbt_unit_testing.mock_ref ('stg_gym_app__height') %}
+    select
+        to_date('2021-07-01', 'YYYY-MM-DD') as created_date,
+        1 as user_id,
+        182 as height
+    UNION ALL
+    select
+        to_date('2022-08-01', 'YYYY-MM-DD') as created_date,
+        1 as user_id,
+        183 as height
+  {% endcall %}
+
+  {% call dbt_unit_testing.expect() %}
+    select
+        to_date('2022/09/01', 'YYYY-MM-DD') as created_date,
+        1 as user_id,
+        82.50 as weight,
+        183 as height
+  {% endcall %}
+
+{% endcall %}
