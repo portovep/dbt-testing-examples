02f362bf11ad5eac3d70ebd7572f853cfe53c096
Pablo <porto.vga@gmail.com>
2023-02-17 09:40:21 +0100

    Make unit test to enrich each weight measurement with the latest height measurement pass

---
+++ models/intermediate/int_weight_measurements_with_latest_height.sql
@@ -0,0 +1,29 @@
+with
+
+height as (
+
+    select * from {{ dbt_unit_testing.ref('stg_gym_app__height') }}
+
+),
+
+weight as (
+
+    select * from {{ dbt_unit_testing.ref('stg_gym_app__weight') }}
+
+),
+
+enrich_with_lasted_height_recorded_before_the_weight_measurement as (
+
+    select distinct on (weight.created_date)
+        weight.created_date,
+        user_id,
+        weight,
+        (select height
+         from height
+         where  height.created_date < weight.created_date
+         order by height.created_date DESC
+         limit 1) as height
+    from weight
+)
+
+select * from enrich_with_lasted_height_recorded_before_the_weight_measurement
