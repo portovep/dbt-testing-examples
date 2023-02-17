f7b507e4bdb5ed85baef3bd7140980f20f8c9e28
Pablo <porto.vga@gmail.com>
2023-02-17 09:25:09 +0100

    Make unit test for converting pounds to kilograms pass

--- models/staging/gym_app/stg_gym_app__weight.sql
+++ models/staging/gym_app/stg_gym_app__weight.sql
@@ -15,7 +15,20 @@ renamed as (
         measurement_unit
     from source
 
+),
+
+converted_to_metric_system as (
+   
+    select
+        created_date,
+        user_id,
+        case when measurement_unit = 'pounds' then round(weight / 2.205)
+            else weight
+        end as weight,
+        'kg' as measurement_unit
+    from renamed
+    
 )
 
-select * from renamed
+select * from converted_to_metric_system
 
