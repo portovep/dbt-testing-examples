4ebdf12979f8ad9c12a862a56d18b5293f476051
Pablo <porto.vga@gmail.com>
2023-02-19 18:06:36 +0100

    Make unit test to calculate body mass indexes pass

--- models/marts/body_mass_indexes.sql
+++ models/marts/body_mass_indexes.sql
@@ -1,1 +1,20 @@
-select 1
+with
+
+weights_with_latest_height as (
+
+    select * from {{ dbt_unit_testing.ref('int_weight_measurements_with_latest_height') }}
+
+),
+
+body_mass_indexes as (
+
+    select
+        created_date,
+        user_id,
+        weight,
+        height,
+        round((weight::numeric / (height::numeric / 100) ^ 2)::numeric, 1) as bmi
+    from weights_with_latest_height
+)
+
+select * from body_mass_indexes
