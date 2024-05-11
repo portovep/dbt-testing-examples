179854e7c845596ed9046e6c84d5681b22c020aa
portovep <portovep@users.noreply.github.com>
2024-05-11 05:36:12 +0000

    deploy: b20c185e15cb00e9b46864e6dc6dfd5febb0ae1f

---
+++ docs/compiled/dbt_testing_example/models/staging/gym_app/_gym_app__raw_height_schema.yml/dbt_expectations_source_expect_89d58c60a30ca33e201a445d2f77dbfa.sql
@@ -0,0 +1,38 @@
+with relation_columns as (
+
+        
+        select
+            cast('DATE' as TEXT) as relation_column,
+            cast('TEXT' as TEXT) as relation_column_type
+        union all
+        
+        select
+            cast('USER_ID' as TEXT) as relation_column,
+            cast('INTEGER' as TEXT) as relation_column_type
+        union all
+        
+        select
+            cast('HEIGHT' as TEXT) as relation_column,
+            cast('DOUBLE PRECISION' as TEXT) as relation_column_type
+        union all
+        
+        select
+            cast('HEIGHT_UNIT' as TEXT) as relation_column,
+            cast('TEXT' as TEXT) as relation_column_type
+        
+        
+    ),
+    test_data as (
+
+        select
+            *
+        from
+            relation_columns
+        where
+            relation_column = 'HEIGHT'
+            and
+            relation_column_type not in ('DOUBLE PRECISION')
+
+    )
+    select *
+    from test_data
