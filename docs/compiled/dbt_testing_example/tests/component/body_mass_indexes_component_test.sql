f5cebf5d99f222b3c6504bf1de548aeb6f375283
portovep <portovep@users.noreply.github.com>
2024-10-15 05:38:43 +0000

    deploy: b20c185e15cb00e9b46864e6dc6dfd5febb0ae1f

---
+++ docs/compiled/dbt_testing_example/tests/component/body_mass_indexes_component_test.sql
@@ -0,0 +1,12 @@
+
+
+
+  
+  
+
+
+  
+    
+      select * from (select 1) as t where 1 = 0
+    
+  
---
+++ docs/compiled/dbt_testing_example/tests/unit/intermediate/int_weight_measurements_with_latest_height_unit_test.sql
@@ -0,0 +1,26 @@
+
+
+
+  
+  
+
+
+  
+    
+      select * from (select 1) as t where 1 = 0
+    
+  
+
+
+UNION ALL
+
+
+  
+  
+
+
+  
+    
+      select * from (select 1) as t where 1 = 0
+    
+  
